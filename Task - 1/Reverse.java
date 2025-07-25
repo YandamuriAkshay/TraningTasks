d303429f5d44fd6e333a27e44a219269fb219d6f
Yandamuri Akshay <88223556+YandamuriAkshay@users.noreply.github.com>
2025-07-25 14:13:13 +0530

    Exercise 5.2

---
+++ Task - 1/Reverse.java
@@ -0,0 +1,10 @@
+public class Reverse {
+    public static void main(String[] args) {
+        String str = args[0];
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        System.out.println(reversed);
+    }
+}
