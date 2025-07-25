26c1188873dc2c64673eb76ec58fc55644e9bbde
Yandamuri Akshay <88223556+YandamuriAkshay@users.noreply.github.com>
2025-07-25 14:19:03 +0530

    Exercise 7.2

---
+++ Task - 1/Task7b.java
@@ -0,0 +1,30 @@
+public class Task7b {
+	byte b;
+	short s;
+	int i;
+	long l;
+	float fl;
+	double dbl;
+	char c;
+	boolean bo;
+	
+	public void predefault() {
+		System.out.println("byte: " + b);
+		System.out.println("short: " + s);
+		System.out.println("int: " + i);
+		System.out.println("long: " + l);
+		System.out.println("float: " + fl);
+		System.out.println("double: " + dbl);
+		System.out.println("char: " + c);
+		System.out.print("Boolean: " + bo);
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Task7b a = new Task7b();
+		a.predefault();
+
+	}
+
+}
