a92f0d24699dec75b9200d0d027ae4ece5894ae9
Yandamuri Akshay <88223556+YandamuriAkshay@users.noreply.github.com>
2025-07-25 16:26:03 +0530

    Exercise 11.1
    Create a string and perform: 
    o Length 
    o Uppercase/Lowercase 
    o Substring 
    o Index of a character

---
+++ Task - 1/Task11a.java
@@ -0,0 +1,13 @@
+public class Task11a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "JavaWorld";
+		
+		System.out.println("length       :  " + str.length());
+		System.out.println("UpperCase    :  " + str.toUpperCase());
+		System.out.println("LowerCase    :  " + str.toLowerCase());
+		System.out.println("SubString(4) :  " + str.substring(4));
+		System.out.println("Index of 'W' :  " + str.indexOf('W'));
+	}
+}
