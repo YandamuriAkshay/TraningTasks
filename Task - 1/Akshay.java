398f599ff9e534cd2a95b0d387c70ab960f7cb20
Yandamuri Akshay <88223556+YandamuriAkshay@users.noreply.github.com>
2025-07-25 14:02:01 +0530

    Exercise 3.1

---
+++ Task - 1/Akshay.java
@@ -0,0 +1,6 @@
+public class Akshay {
+    public static void main(String[] args) {
+        System.out.println("Hello, Akshay!");
+        System.out.println("Today is a great day to code Java!");
+    }
+}
