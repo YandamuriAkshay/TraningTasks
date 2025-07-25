dfcca408b6ca8e4f033668efeec44e8e214e0903
Yandamuri Akshay <88223556+YandamuriAkshay@users.noreply.github.com>
2025-07-25 14:15:19 +0530

    Exercise 7.1

---
+++ Task - 1/Task7a.java
@@ -0,0 +1,25 @@
+public class Task7a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte b = 127;
+		short s = 32000;
+		int i = 1005435000;
+		long l = 05470302770;
+		float fl = 4500;
+		double dbl = 678.587;
+		char c = 'A';
+		Boolean bo = true;
+		
+		System.out.println("byte: " + b);
+		System.out.println("short: " + s);
+		System.out.println("int: " + i);
+		System.out.println("long: " + l);
+		System.out.println("float: " + fl);
+		System.out.println("double: " + dbl);
+		System.out.println("char: " + c);
+		System.out.print("Boolean: " + bo);
+
+	}
+
+}
