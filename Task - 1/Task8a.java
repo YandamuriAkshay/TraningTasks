3632a7fffe51309f0b96c4b13a1bc92ffb2d2559
Yandamuri Akshay <88223556+YandamuriAkshay@users.noreply.github.com>
2025-07-25 14:20:38 +0530

    Exercise 8.1

---
+++ Task - 1/Task8a.java
@@ -0,0 +1,37 @@
+public class Task8a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n1 = 10;
+		int n2 = 5;
+		
+		int add;
+		add = n1 + n2;
+		
+		int sub;
+		sub = n1 - n2;
+		
+		int multi;
+		multi = n1 * n2;
+		
+		int divi;
+		divi = n1 / n2;
+		
+		System.out.println("Addition: " + add);
+		System.out.println("Subtraction: " + sub);
+		System.out.println("Multipication: " + multi);
+		System.out.println("Division: " + divi);
+		
+		//Ass
+		
+		n1 += n2;
+		System.out.println(n1);
+		
+		n1 *= n2;
+		System.out.println(n1);
+		
+		
+		
+	}
+
+}
